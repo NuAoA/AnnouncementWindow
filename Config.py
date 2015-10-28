6a772d5fd38d200b67a58080c324b320b096dda1
NuAoA <wiez5@hotmail.com>
2015-10-28 17:47:20 -0400

    Fixed issue with loading bitmaps on linux

--- Config.py
+++ Config.py
@@ -19,7 +19,7 @@ class config(object):
         self.filters_path =        "filters.txt"    
         self.gui_data =            "Data/gui.dat"        
         self.filters_pickle_path = "Data/filters.dat"
-        self.icon_path = "Data/favicon.XBM" if platform_linux else "Data/favicon.ico"   
+        self.icon_path = "@Data/favicon.XBM" if platform_linux else "Data/favicon.ico"   
         self.section ="Settings"
         self.init_var()
         self.load() 
